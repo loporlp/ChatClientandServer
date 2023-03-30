56c4f452cb74ab619722decfb292276f1b9abde7
Mason Sansom <masonsansom02@gmail.com>
2023-03-30 17:12:40 -0600

    Added GUI Elements

--- Networking_and_Logging/ChatClient/MainPage.xaml.cs
+++ Networking_and_Logging/ChatClient/MainPage.xaml.cs
@@ -1,26 +1,54 @@
 ﻿using Communications;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace ChatClient;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+   private ObservableCollection<string> clientList;
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    public MainPage()
+    {
+        InitializeComponent();
+        //	Networking channel = new Networking(NullLogger.Instance, onConnect(), onDisconnect(), onMessage(), '.');
+        clientList = new ObservableCollection<string>{ "Maosn", "1 Hour", "1:30 Hour", "2 Hours" };
+        ClientList.ItemsSource = clientList;
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    ReportMessageArrived onMessage()
+    {
+        throw new NotImplementedException();
+    }
+
+    ReportConnectionEstablished onConnect()
+    {
+        throw new NotImplementedException();
+    }
+
+    ReportDisconnect onDisconnect()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void addressEntered(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void connectToServer(object sender, EventArgs e)
+    {
+        clientList.Add("SDFS");
+       // ClientList.ItemsSource = clientList;
+    }
+
+    private void nameEntered(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 }
 
